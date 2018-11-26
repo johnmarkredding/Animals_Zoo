878811c716ac7899764a986b196e102ea5e7d761
John Mark Redding <johomurk@gmail.com>
2018-11-26 15:30:53 -0500

    Coo

--- run.rb
+++ run.rb
@@ -1,10 +1,24 @@
-require_relative "lib/Animal.rb"
-require_relative "lib/Zoo.rb"
+require_relative "./lib/Animal.rb"
+require_relative "./lib/Zoo.rb"
 require 'pry'
 
 
 #Test your code here
+lower = Zoo.new("lower", "manhattan")
+mid = Zoo.new("mid", "manhattan")
+upper = Zoo.new("upper", "manhattan")
 
+lower.new_animal("cat", 35, "Pebbles")
+mid.new_animal("cat", 35, "Pebbles")
+upper.new_animal("cat", 35, "Pebbles")
+lower.new_animal("dog", 5, "Teensy")
+mid.new_animal("dog", 5, "Teensy")
+upper.new_animal("dog", 5, "Teensy")
+lower.new_animal("T-Rex", 20000, "Sarcy")
+mid.new_animal("T-Rex", 20000, "Sarcy")
+upper.new_animal("T-Rex", 20000, "Sarcy")
+
+Zoo.all
 
 binding.pry
 puts "done"
